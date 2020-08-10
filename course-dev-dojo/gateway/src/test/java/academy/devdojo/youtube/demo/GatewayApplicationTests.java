b8bb09b97db273e1b7387b663a782da09a025176
jeffersonacm <jeffersonacm@hotmail.com>
2020-08-09 22:54:40 -0300

    Add - service discovery and gateway zuul

---
+++ course-dev-dojo/gateway/src/test/java/academy/devdojo/youtube/demo/GatewayApplicationTests.java
@@ -0,0 +1,11 @@
+package academy.devdojo.youtube.demo;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    void contextLoads() {
+    }
+
+}
